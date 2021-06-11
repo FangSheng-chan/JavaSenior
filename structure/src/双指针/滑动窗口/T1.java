a1da395470b2a141818d1b007fbca91030fe08dd
fangsheng <445317262@qq.com>
2021-06-11 14:43:17 +0800

    算法

--- structure/src/双指针/滑动窗口/T1.java
+++ structure/src/双指针/滑动窗口/T1.java
@@ -1,6 +1,8 @@
 package 双指针.滑动窗口;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -125,4 +127,59 @@ public class T1 {
         // 返回最小覆盖子串
         return false;
     }
+
+    /**
+     * 跟寻找字符串的排列一样，只是找到一个合法异位词（排列）之后将起始索引加入 res 即可。
+     * @param s
+     * @param t
+     * @return
+     */
+    public static List<Integer> checkInclusion2(String s, String t) {
+        HashMap<Character, Integer> need = new HashMap<>();
+        HashMap<Character, Integer> window = new HashMap<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        int t_len = t.length();
+        for (int i = 0; i < t_len; i++) {
+            need.put(t.charAt(i), need.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        int left = 0, right = 0;
+        int valid = 0;
+        // 记录最小覆盖子串的起始索引及⻓度
+        int start = 0, len = Integer.MAX_VALUE;
+        while (right < s.length()) {
+            // c 是将移入窗口的字符
+            char c = s.charAt(right); // 右移窗口
+            right++;
+            // 进行窗口内数据的一系列更新
+            if (need.containsKey(c)) {
+                int tmp = window.getOrDefault(c, 0);
+                window.put(c, ++tmp);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+            // 判断左侧窗口是否要收缩
+            while (right-left>=t_len) {
+                // 在这里更新最小覆盖子串
+                if (valid==need.size()&&!s.substring(left,right).equals(t)) {
+                    res.add(left);
+                }
+                // d 是将移出窗口的字符
+                char d = s.charAt(left);
+                // 左移窗口
+                left++;
+                // 进行窗口内数据的一系列更新
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    int tmp = window.get(d);
+                    window.put(d, --tmp);
+                }
+            }
+        }
+        // 返回最小覆盖子串
+        return res;
+    }
+
 }
